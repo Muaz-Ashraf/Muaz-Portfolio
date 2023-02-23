27143d4a870e52bd84004816570fb7ac50fe27ba
Muaz Ashraf <muazashrafnet@gmail.com>
2023-02-23 05:07:02 +0500

    add copy button to contact info

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+  document.querySelector("#copy-btn-1").addEventListener("click", () => {
+    navigator.clipboard.writeText(
+      document.querySelector("#copy-number").innerHTML
+    );
+  });
+  document.querySelector("#copy-btn-2").addEventListener("click", () => {
+    navigator.clipboard.writeText(
+      document.querySelector("#copy-email").innerHTML
+    );
+  });
 })();
